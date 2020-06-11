6778020f0936c1853463f57ac2f3009dbbbeafbf
Rachel S. Lee <rslee0025@gmail.com>
2020-06-11 22:53:17 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,7 +3,7 @@ class Backer
   attr_accessor :backed_projects
   attr_reader :name, :project
 
-  def initialize(name, title)
+  def initialize(name)
     @name = name 
     @backed_projects = []
     @project = project
